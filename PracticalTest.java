3547f89970686a592bbba2877892202abf587fe0
IlonaVevere <vevere.ilona@gmail.com>
2019-04-05 20:28:45 +0300

    Task 2

--- PracticalTest.java
+++ PracticalTest.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 
-public class PracticalTest {
+public class Task2 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-	System.out.println ("Hello, dear guest! What is your name?");
-	String guest = sc.nextLine();
-	System.out.println("Peasure to meet you " + guest + " you are welcome to the opening ceremony!");
+	System.out.println("How much people are in the room?");
+	int inRoom = sc.nextInt();
+	System.out.println("How much people are waiting in the queue?");
+	int inQueue = sc.nextInt();
+	
+	int freeSpace = 100-inRoom;
+	int cant = inQueue-freeSpace;
+	String postfix = cant ==1 ? "quest" : "guests";
+	
+	if (inQueue <= freeSpace){
+	System.out.println("All waiting quests can fit in the room.");
+	} else System.out.println("Can`t fit "+ cant + " " + postfix);
+	
 	}
  }
