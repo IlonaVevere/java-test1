126f3a484099519bb841c03ce8d3772b4f8b5f16
IlonaVevere <vevere.ilona@gmail.com>
2019-04-05 19:58:55 +0300

    Task 1

---
+++ PracticalTest.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PracticalTest {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+	System.out.println ("Hello, dear guest! What is your name?");
+	String guest = sc.nextLine();
+	System.out.println("Peasure to meet you " + guest + " you are welcome to the opening ceremony!");
+	}
+ }
